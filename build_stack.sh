eef7128e7bd904f1349b538f1b1fef8419f31fcd
Andres Montealegre <andres.montealegre@oracle.com>
2024-01-24 18:27:55 -0500

    initial version

---
+++ build_stack.sh
@@ -0,0 +1,21 @@
+file_path=$1
+
+for url in $(grep -Eo 'source\s+=\s+"([^"]+)"' "$file_path" | awk -F'"' '{print $2}'); do
+  tag_number=$(echo "$url" | awk -F'=' '{print $2}')
+  clean_url=$(echo "$url" | awk -F'::|?|//' '{print $2"//"$3}')
+  echo "Cloning repository: $clean_url (Tag: $tag_number)"
+  git clone --branch "$tag_number" --single-branch --depth 1 "$clean_url" > /dev/null 2> /dev/null &
+done
+
+echo "Updating sources..."
+sed -i -E 's|source\s+=\s+"git::[^"]+/terraform-(.+)\?ref=.+"|source = "./terraform-\1"|; s|\.git//|/|; s|\.git|/|' "$file_path"
+
+echo "Updating provider..."
+cat <<EOL > provider.tf
+provider "oci" {
+  auth   = "InstancePrincipal"
+  region = var.region
+}
+EOL
+
+zip -r stack.zip . -x "*.terraform*" ".terraform/*" ".git/*" "backend.tf"
---
+++ deploy_stack.sh
@@ -0,0 +1,21 @@
+STACK_NAME=$1
+COMPARTMENT=$2
+
+export COMPARTMENT_OCID=$(oci iam compartment list --compartment-id-in-subtree true --all --output json | jq -r --arg name "$COMPARTMENT" '.data | map(select(.name == $name))[0]?.id')
+
+export STACK_ID=$(oci resource-manager stack list --all --compartment-id $COMPARTMENT_OCID | jq ".data[] | select(.\"display-name\"==\"$STACK_NAME\").id" -r)
+zip -r stack.zip . -x "*.terraform*" ".terraform/*" ".git/*" "backend.tf"
+if [ -z "$STACK_ID" ]; then
+    export STACK_ID=$(oci resource-manager stack create \
+    --compartment-id $COMPARTMENT_OCID \
+    --config-source stack.zip \
+    --display-name "$STACK_NAME" \
+    --wait-for-state ACTIVE | jq '.data.id' -r)
+else
+    oci resource-manager stack update \
+    --stack-id $STACK_ID \
+    --config-source stack.zip \
+    --force
+fi
+oci resource-manager job create-plan-job \
+    --stack-id $STACK_ID
