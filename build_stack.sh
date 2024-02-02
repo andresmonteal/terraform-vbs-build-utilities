7d91d58ab158a50a2721f09759dc7f382d60fe90
Andres Montealegre <andres.montealegre@oracle.com>
2024-02-02 15:09:50 -0500

    fix: avoid repeated urls

--- build_stack.sh
+++ build_stack.sh
@@ -1,11 +1,17 @@
 file_path=$1
 
+processed_urls=()  # Initialize an empty array to store processed URLs
+
 for url in $(grep -Eo 'source\s+=\s+"([^"]+)"' "$file_path" | awk -F'"' '{print $2}'); do
   tag_number=$(echo "$url" | awk -F'=' '{print $2}')
   clean_url=$(echo "$url" | awk -F'::|?|//' '{print $2"//"$3}')
-  echo "Cloning repository: $clean_url (Tag: $tag_number)"
-  echo "Cloning repository: $clean_url (Tag: $tag_number)"  >>  versions
-  git clone --branch "$tag_number" --single-branch --depth 1 "$clean_url"
+  # Check if the URL has already been processed
+  if [[ ! " ${processed_urls[@]} " =~ " $clean_url " ]]; then
+    echo "Cloning repository: $clean_url (Tag: $tag_number)"
+    echo "Cloning repository: $clean_url (Tag: $tag_number)"  >>  versions
+    git clone --branch "$tag_number" --single-branch --depth 1 "$clean_url"
+    processed_urls+=("$clean_url")  # Add the URL to the processed list
+  fi
 done
 
 echo "Updating sources..."
